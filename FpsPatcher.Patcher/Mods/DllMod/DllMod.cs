944990713dd41fe9eab603ec031ca596b0867315
KulaGGin <sergeant.coolagin@gmail.com>
2020-07-20 22:32:58 +0300

    Fill DllMod.
    Add ApplyMod method.

--- FpsPatcher.Patcher/Mods/DllMod/DllMod.cs
+++ FpsPatcher.Patcher/Mods/DllMod/DllMod.cs
@@ -6,10 +6,13 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Win32;
 using Patcher.Mods;
+using AsmResolver.X86;
 
 
 namespace FpsPatcher.Patcher.Mods.DllMod {
     public class DllMod : Mod {
+        private const string _fpsPatcherDllName = "FpsPatcher.Internal.dll";
+        private readonly string _fpsPatcherDllPath = AppDomain.CurrentDomain.BaseDirectory + "\\" + "FpsPatcher.Internal.dll";
         public DllMod() {
 
         }
@@ -17,16 +20,25 @@ namespace FpsPatcher.Patcher.Mods.DllMod {
         #region Overrides of Mod
 
         public override void ApplyMod() {
-
-            throw new NotImplementedException();
         }
 
         public override void CancelMod() {
-            throw new NotImplementedException();
         }
 
         #endregion
 
+        public void ApplyMod(string tekkenShippingExePath) {
+            string tekkenShippingFolder = Path.GetDirectoryName(tekkenShippingExePath);
 
+            if (!File.Exists(_fpsPatcherDllPath)) {
+                throw new Exception("Can't find" + _fpsPatcherDllName + ".");
+            }
+
+            File.Copy(_fpsPatcherDllPath, tekkenShippingFolder + "\\" + _fpsPatcherDllName);
+
+            var peFile = new Pe
+            peFile.AddImport("_fpsPatcherDllName", "StartPage");
+
+        }
     }
 }
