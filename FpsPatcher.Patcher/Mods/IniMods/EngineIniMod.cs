3a5517c1a90d22ce1fc38b0e87169415b35b9f90
KulaGGin <sergeant.coolagin@gmail.com>
2020-07-20 15:03:35 +0300

    Rename IniMods to Ini Mods folder.

---
+++ FpsPatcher.Patcher/Mods/IniMods/EngineIniMod.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Patcher.Mods;
+
+
+namespace FpsPatcher.Patcher.Mods.IniMods {
+    public abstract class EngineIniMod : Mod {
+
+        protected readonly string _engineIniPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + "\\TekkenGame\\Saved\\Config\\WindowsNoEditor\\Engine.ini";
+
+
+    }
+}
