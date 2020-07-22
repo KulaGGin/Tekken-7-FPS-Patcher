3a0180179ddcc2e6b93d7c33850a3f94230dc215
KulaGGin <sergeant.coolagin@gmail.com>
2020-07-22 23:11:56 +0300

    Refactor DllMod class.
    Fix issues in the code.
    Use explicit types instead of var.
    Use full names instead of abbreviations.
    Remove redundant default empty constructor.
    Call AddImport instead of AddImports function of the PeFile because I add only one import.

--- FpsPatcher.Patcher/Mods/DllMod/DllMod.cs
+++ FpsPatcher.Patcher/Mods/DllMod/DllMod.cs
@@ -9,16 +9,12 @@ using FPSPatcher.Managers;
 using Microsoft.Win32;
 using Patcher.Mods;
 using PeNet;
-using PeNet.Header.Pe;
 
 
 namespace FPSPatcher.Patcher.Mods.DllMod {
     public class DllMod : Mod {
         private const string _fpsPatcherDllName = "FPSPatcher.Internal.dll";
         private readonly string _fpsPatcherDllPath = AppDomain.CurrentDomain.BaseDirectory + "\\" + "FPSPatcher.Internal.dll";
-        public DllMod() {
-
-        }
 
         #region Overrides of Mod
 
@@ -48,21 +44,20 @@ namespace FPSPatcher.Patcher.Mods.DllMod {
             // todo create backup of the original tekkenShippingExe file
             // load the target file and change size so it's aligned by 0x10000
             List<byte> tekkenExeBytes = File.ReadAllBytes(tekkenShippingExePath).ToList();
-            var neededForAlignment = 0x10000 - tekkenExeBytes.Count % 0x10000;
+            int neededForAlignment = 0x10000 - tekkenExeBytes.Count % 0x10000;
             tekkenExeBytes.AddRange(Enumerable.Repeat<byte>(0x00, neededForAlignment));
             // open the file in PeFile
-            var tekkenExePeFile = new PeNet.PeFile(tekkenExeBytes.ToArray());
+            PeFile tekkenExePeFile = new PeFile(tekkenExeBytes.ToArray());
 
-            bool isDllInjected = tekkenExePeFile.ImportedFunctions.ToList().Any(function => function.DLL == _fpsPatcherDllName);
+            bool isDllInjected = (tekkenExePeFile.ImportedFunctions ?? throw new InvalidOperationException("Imported functions array is null")).ToList().Any(function => function.DLL == _fpsPatcherDllName);
+
             if (isDllInjected)
                 return;
 
             //add our new import to the exe (in memory)
-            var ai = new AdditionalImport(_fpsPatcherDllName, new List<string> { "?FPSPatchInternal@@YAKPEAUHINSTANCE__@@@Z" });
-            tekkenExePeFile.AddImports(new List<AdditionalImport> { ai });
+            tekkenExePeFile.AddImport(_fpsPatcherDllName, "?FPSPatchInternal@@YAKPEAUHINSTANCE__@@@Z");
             //write changes to disk
             File.WriteAllBytes(tekkenShippingExePath, tekkenExePeFile.RawFile.ToArray());
-
             // todo write that we're done with the patching
         }
     }
